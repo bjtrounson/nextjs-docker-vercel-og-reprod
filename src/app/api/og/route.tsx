5938039be8d166b4bbb8b8449bde2a108c0f5c81
Benjamin Trounson <benjamint@contecgroup.com>
2025-01-08 11:36:13 +1300

    Docker and vercel og image generation

---
+++ src/app/api/og/route.tsx
@@ -0,0 +1,491 @@
+import { ImageResponse } from "next/og";
+
+export const dynamic = "force-dynamic";
+export const runtime = "nodejs";
+
+const size = {
+  width: 270,
+  height: 1200,
+};
+
+export async function GET() {
+  const color = "#26D07C";
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "transparent",
+        }}
+      >
+        <svg
+          width="26.994368"
+          height="120"
+          viewBox="0 0 26.994368 120"
+          version="1.1"
+          id="svg5"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <defs id="defs2" />
+          <g
+            id="g17355"
+            transform="matrix(-0.6191386,0,0,-0.65847165,72.853903,131.99967)"
+          >
+            <rect
+              style={{
+                fill: "#000000",
+                fillOpacity: 1,
+                stroke: "none",
+                strokeWidth: 4.09365,
+                strokeLinecap: "round",
+                strokeLinejoin: "miter",
+                strokeDasharray: "none",
+                strokeOpacity: 1,
+                paintOrder: "normal",
+              }}
+              id="rect16106-7"
+              width="3.9786987"
+              height="15.867881"
+              x="74.069901"
+              y="31.767267"
+              ry="1.4647274"
+              rx="1.9893494"
+            />
+            <rect
+              style={{
+                fill: "#000000",
+                fillOpacity: 1,
+                stroke: "none",
+                strokeWidth: 4.09365,
+                strokeLinecap: "round",
+                strokeLinejoin: "miter",
+                strokeDasharray: "none",
+                strokeOpacity: 1,
+                paintOrder: "normal",
+              }}
+              id="rect16106-7-0"
+              width="3.9786987"
+              height="15.867881"
+              x="74.078728"
+              y="50.267174"
+              ry="1.4647274"
+              rx="1.9893494"
+            />
+            <rect
+              style={{
+                fill: "#000000",
+                fillOpacity: 1,
+                stroke: "none",
+                strokeWidth: 4.09365,
+                strokeLinecap: "round",
+                strokeLinejoin: "miter",
+                strokeDasharray: "none",
+                strokeOpacity: 1,
+                paintOrder: "normal",
+              }}
+              id="rect16106-7-7"
+              width="3.9786987"
+              height="15.867881"
+              x="74.088852"
+              y="68.766945"
+              ry="1.4647274"
+              rx="1.9893494"
+            />
+            <rect
+              style={{
+                fill: "#000000",
+                fillOpacity: 1,
+                stroke: "none",
+                strokeWidth: 4.09365,
+                strokeLinecap: "round",
+                strokeLinejoin: "miter",
+                strokeDasharray: "none",
+                strokeOpacity: 1,
+                paintOrder: "normal",
+              }}
+              id="rect16106-7-4"
+              width="3.9786987"
+              height="15.867881"
+              x="74.076241"
+              y="87.290802"
+              ry="1.4647274"
+              rx="1.9893494"
+            />
+            <rect
+              style={{
+                fill: "#000000",
+                fillOpacity: 1,
+                stroke: "none",
+                strokeWidth: 4.09365,
+                strokeLinecap: "round",
+                strokeLinejoin: "miter",
+                strokeDasharray: "none",
+                strokeOpacity: 1,
+                paintOrder: "normal",
+              }}
+              id="rect16106-7-43"
+              width="3.9786987"
+              height="15.867881"
+              x="74.079735"
+              y="133.61617"
+              ry="1.4647274"
+              rx="1.9893494"
+            />
+            <rect
+              style={{
+                fill: "#000000",
+                fillOpacity: 1,
+                stroke: "none",
+                strokeWidth: 4.09365,
+                strokeLinecap: "round",
+                strokeLinejoin: "miter",
+                strokeDasharray: "none",
+                strokeOpacity: 1,
+                paintOrder: "normal",
+              }}
+              id="rect16106-7-9"
+              width="3.9786987"
+              height="15.867881"
+              x="74.089279"
+              y="168.01047"
+              ry="1.4647274"
+              rx="1.9893494"
+            />
+            <g id="g16257" transform="translate(41.867252,-66.91547)">
+              <rect
+                style={{
+                  fill: "#000000",
+                  fillOpacity: 1,
+                  stroke: "none",
+                  strokeWidth: 4.09365,
+                  strokeLinecap: "round",
+                  strokeLinejoin: "miter",
+                  strokeDasharray: "none",
+                  strokeOpacity: 1,
+                  paintOrder: "normal",
+                }}
+                id="rect16106-7-1"
+                width="3.9786987"
+                height="15.867881"
+                x="71.804451"
+                y="98.668152"
+                ry="1.4647274"
+                rx="1.9893494"
+              />
+              <rect
+                style={{
+                  fill: "#000000",
+                  fillOpacity: 1,
+                  stroke: "none",
+                  strokeWidth: 4.09365,
+                  strokeLinecap: "round",
+                  strokeLinejoin: "miter",
+                  strokeDasharray: "none",
+                  strokeOpacity: 1,
+                  paintOrder: "normal",
+                }}
+                id="rect16106-7-0-5"
+                width="3.9786987"
+                height="15.867881"
+                x="71.813278"
+                y="117.16806"
+                ry="1.4647274"
+                rx="1.9893494"
+              />
+              <rect
+                style={{
+                  fill: "#000000",
+                  fillOpacity: 1,
+                  stroke: "none",
+                  strokeWidth: 4.09365,
+                  strokeLinecap: "round",
+                  strokeLinejoin: "miter",
+                  strokeDasharray: "none",
+                  strokeOpacity: 1,
+                  paintOrder: "normal",
+                }}
+                id="rect16106-7-7-0"
+                width="3.9786987"
+                height="15.867881"
+                x="71.823402"
+                y="135.66783"
+                ry="1.4647274"
+                rx="1.9893494"
+              />
+              <rect
+                style={{
+                  fill: "#000000",
+                  fillOpacity: 1,
+                  stroke: "none",
+                  strokeWidth: 4.09365,
+                  strokeLinecap: "round",
+                  strokeLinejoin: "miter",
+                  strokeDasharray: "none",
+                  strokeOpacity: 1,
+                  paintOrder: "normal",
+                }}
+                id="rect16106-7-4-1"
+                width="3.9786987"
+                height="15.867881"
+                x="71.810791"
+                y="154.19168"
+                ry="1.4647274"
+                rx="1.9893494"
+              />
+              <rect
+                style={{
+                  fill: "#000000",
+                  fillOpacity: 1,
+                  stroke: "none",
+                  strokeWidth: 4.09365,
+                  strokeLinecap: "round",
+                  strokeLinejoin: "miter",
+                  strokeDasharray: "none",
+                  strokeOpacity: 1,
+                  paintOrder: "normal",
+                }}
+                id="rect16106-7-43-2"
+                width="3.9786987"
+                height="15.867881"
+                x="71.814285"
+                y="200.51704"
+                ry="1.4647274"
+                rx="1.9893494"
+              />
+              <rect
+                style={{
+                  fill: "#000000",
+                  fillOpacity: 1,
+                  stroke: "none",
+                  strokeWidth: 4.09365,
+                  strokeLinecap: "round",
+                  strokeLinejoin: "miter",
+                  strokeDasharray: "none",
+                  strokeOpacity: 1,
+                  paintOrder: "normal",
+                }}
+                id="rect16106-7-9-9"
+                width="3.9786987"
+                height="15.867881"
+                x="71.82383"
+                y="234.91135"
+                ry="1.4647274"
+                rx="1.9893494"
+              />
+            </g>
+            <rect
+              style={{
+                fill: "#201747",
+                fillOpacity: 1,
+                stroke: "#201700",
+                strokeWidth: 1.29187,
+                strokeLinecap: "round",
+                strokeLinejoin: "round",
+                strokeDasharray: "none",
+                strokeOpacity: 1,
+                paintOrder: "normal",
+              }}
+              id="rect7067"
+              width="38.627331"
+              height="143.55408"
+              x="76.613457"
+              y="21.961357"
+              rx="0"
+              ry="0"
+            />
+            <rect
+              style={{
+                fill: color,
+                fillOpacity: 1,
+                stroke: "#201700",
+                strokeWidth: 1.19219,
+                strokeLinecap: "round",
+                strokeLinejoin: "round",
+                strokeDasharray: "none",
+                strokeOpacity: 1,
+                paintOrder: "normal",
+              }}
+              id="rect234"
+              width="34.388588"
+              height="142.84392"
+              x="78.677437"
+              y="18.819616"
+              ry="3.0114036"
+              rx="17.194294"
+            />
+            <rect
+              style={{
+                fill: color,
+                fillOpacity: 1,
+                stroke: "#201700",
+                strokeWidth: 1.32776,
+                strokeLinecap: "round",
+                strokeLinejoin: "round",
+                strokeDasharray: "none",
+                strokeOpacity: 1,
+                paintOrder: "normal",
+              }}
+              id="rect5520"
+              width="21.16132"
+              height="13.222866"
+              x="85.330818"
+              y="37.707577"
+              rx="0"
+              ry="0"
+            />
+            <rect
+              style={{
+                fill: color,
+                fillOpacity: 1,
+                stroke: "#201700",
+                strokeWidth: 1.32776,
+                strokeLinecap: "round",
+                strokeLinejoin: "round",
+                strokeDasharray: "none",
+                strokeOpacity: 1,
+                paintOrder: "normal",
+              }}
+              id="rect5520-6"
+              width="21.16132"
+              height="13.222866"
+              x="85.330795"
+              y="131.63754"
+              rx="0"
+              ry="0"
+            />
+            <rect
+              style={{
+                fill: color,
+                fillOpacity: 1,
+                stroke: "#201700",
+                strokeWidth: 1.32776,
+                strokeLinecap: "round",
+                strokeLinejoin: "round",
+                strokeDasharray: "none",
+                strokeOpacity: 1,
+                paintOrder: "normal",
+              }}
+              id="rect5520-6-7"
+              width="21.16132"
+              height="13.222866"
+              x="85.375275"
+              y="83.99955"
+              rx="0"
+              ry="0"
+            />
+            <g
+              id="g15937"
+              transform="matrix(0.99773452,0,0,1.1441803,0.21730459,-23.781756)"
+            >
+              <g id="g15998">
+                <rect
+                  style={{
+                    fill: "#FFFFFF",
+                    fillOpacity: 1,
+                    stroke: "#201700",
+                    strokeWidth: 1.25776,
+                    strokeLinecap: "round",
+                    strokeLinejoin: "miter",
+                    strokeDasharray: "none",
+                    strokeOpacity: 1,
+                    paintOrder: "normal",
+                  }}
+                  id="rect10025"
+                  width="38.755001"
+                  height="11.982445"
+                  x="76.55098"
+                  y="165.50992"
+                  ry="0"
+                />
+                <rect
+                  style={{
+                    fill: "#FFFFFF",
+                    fillOpacity: 1,
+                    stroke: "#201700",
+                    strokeWidth: 1.12828,
+                    strokeLinecap: "round",
+                    strokeLinejoin: "round",
+                    strokeDasharray: "none",
+                    strokeOpacity: 1,
+                    paintOrder: "normal",
+                  }}
+                  id="rect10025-7"
+                  width="38.867386"
+                  height="4.2233629"
+                  x="76.480232"
+                  y="191.19022"
+                  ry="0"
+                />
+                <rect
+                  style={{
+                    fill: "#cbf3d9",
+                    fillOpacity: 1,
+                    stroke: "#201700",
+                    strokeWidth: 1.12246,
+                    strokeLinecap: "round",
+                    strokeLinejoin: "miter",
+                    strokeDasharray: "none",
+                    strokeOpacity: 1,
+                    paintOrder: "normal",
+                  }}
+                  id="rect11531"
+                  width="38.871902"
+                  height="10.498144"
+                  x="76.479034"
+                  y="180.95238"
+                />
+                <rect
+                  style={{
+                    fill: "#cbf3d9",
+                    fillOpacity: 1,
+                    stroke: "#201700",
+                    strokeWidth: 1.25977,
+                    strokeLinecap: "round",
+                    strokeLinejoin: "miter",
+                    strokeDasharray: "none",
+                    strokeOpacity: 1,
+                    paintOrder: "normal",
+                  }}
+                  id="rect11531-1"
+                  width="38.73003"
+                  height="3.516958"
+                  x="76.550545"
+                  y="177.50404"
+                />
+                <rect
+                  style={{
+                    fill: "#FFFFFF",
+                    fillOpacity: 1,
+                    stroke: "#201700",
+                    strokeWidth: 1.27645,
+                    strokeLinecap: "round",
+                    strokeLinejoin: "miter",
+                    strokeDasharray: "none",
+                    strokeOpacity: 1,
+                    paintOrder: "normal",
+                  }}
+                  id="rect15991"
+                  width="13.301583"
+                  height="1.8884723"
+                  x="89.255531"
+                  y="193.46118"
+                />
+              </g>
+            </g>
+          </g>
+        </svg>
+      </div>
+    ),
+    {
+      ...size,
+      headers: {
+        "Content-Type": "image/webp",
+      },
+    }
+  );
+}
